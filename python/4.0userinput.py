e027b812b7781572b89d610ba09e5496aa5a4f03
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-09 17:22:50 +0530

    taking user input

---
+++ python/4.0userinput.py
@@ -0,0 +1,14 @@
+#user input 
+
+input("enter your name :")
+
+#but we are not doing anything with this input 
+
+name = ("Input your first name :")
+print(name)
+
+#now the input takes string as the default input therefore to get input of say a number we typecaste it 
+
+age = int(input("Enter your age : "))
+gpa = float(input("Enter your gpa : "))
+
