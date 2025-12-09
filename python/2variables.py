85e75fbb3f0737351d481c7f6fcbe2793370fbd6
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-09 12:05:19 +0530

    Basic variable types in python

---
+++ python/2variables.py
@@ -0,0 +1,21 @@
+#variable - a container that stores value
+
+#strings
+first_name = "Aarush"
+favourite_food = "Chaap"
+
+print(f"hello, {first_name}")
+print(f"your favourite food is {favourite_food}")
+
+#integers
+
+age = 19
+print(f"you are {age} years old")
+
+#float 
+
+cgpa = 9.78
+print(f"your cgpa is {cgpa}")
+
+#boolean
+#boolean is mainly used internally as condition it is either True or False
