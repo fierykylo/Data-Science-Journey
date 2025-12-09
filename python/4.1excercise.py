2fd78cc614f344213e94f286c99e05f2bc3d1fea
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-09 17:17:45 +0530

    user input ex -1

---
+++ python/4.1excercise.py
@@ -0,0 +1,7 @@
+#Excercise - 1 calculating the area of a rectangle
+
+length = float(input("Enter the length of the rectangle : "))
+breadth = float(input("Enter the breadth of the rectangle : "))
+
+area = length * breadth
+print(f"The area of the given rectangle is {area}cm²")
