ea904c6e9835691bc092b55f967cc34830c085b5
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-10 10:05:49 +0530

    some inbuilt math functions

---
+++ python/6.1arithmetic.py
@@ -0,0 +1,17 @@
+#this will cover some inbuilt math functions 
+
+x = 3.14
+y = -4
+z = 5
+
+result = round(x)
+result1 = abs(y)
+power = pow(2,4)
+maximum = max(x, y, z)
+minimum = min(x, y, z)
+
+print(result)
+print(result1)
+print(power)
+print(maximum)
+print(minimum)
