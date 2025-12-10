13e5856091aa291bc65e1207bf355394cd80ea3f
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-10 10:08:28 +0530

    some cool functions from the math module

---
+++ python/6.2arithmetic.py
@@ -0,0 +1,12 @@
+#in this we will use functions from the math module
+
+import math
+
+print(math.pi)
+print(math.e) #exponential constant
+result = math.sqrt(9)
+ceiling = math.ceil(9.1)
+floorvalue = math.floor(9.9)
+print(result)
+print(ceiling)
+print(floorvalue)
