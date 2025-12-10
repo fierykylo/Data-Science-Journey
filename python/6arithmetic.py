d74e0d06b886f667575c74db20903a9a6a8beef0
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-10 09:49:04 +0530

    simple basic arithmetic opartors in python

---
+++ python/6arithmetic.py
@@ -0,0 +1,15 @@
+#this is how maths is done in python 
+
+#basically  like in cpp or c you dont have the variable++ but you do have the syntatic sugar
+
+rand = 20
+rand += 1
+print(rand)
+rand -= 1
+print(rand)
+rand /= 5 # by default gives the float value
+print(rand)
+rand *= 2
+print(rand)
+rand %= 1 #modulus opearator returns the remainder
+print(rand)
