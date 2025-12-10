b0e36a9d92548372cdd6b630079ba84ab4477d86
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-10 10:14:02 +0530

    the area of a circle

---
+++ python/6.4arithmetic.py
@@ -0,0 +1,7 @@
+#area of a circle also super easy
+
+import math as m
+radius = float(input("Enter the radius of athe circle : "))
+
+area = m.pi * pow(radius, 2)
+print(f"The area of the circle is {round(area, 2)}cm^2")
