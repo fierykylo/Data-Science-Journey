082a23df62c25af817213a8123193c84750cb55a
Aarush Dubey <mrknownunknown2702@gmail.com>
2025-12-10 16:02:42 +0530

    Some string methods

---
+++ python/13.0stringmethods.py
@@ -0,0 +1,28 @@
+#string methods
+
+name = "Aarush Dubey"
+length = len(name)
+print(length)
+
+
+name = name.capitalize()
+print(name)
+
+result = name.find("u")
+print(result)
+reuslt = name.rfind("u")
+print(result)
+result = name.count("u")
+print(result)
+name = name.replace("h", "")
+print(name)
+
+
+random = "MaLAI chaaP"
+random = random.lower()
+print(random)
+
+random = random.upper()
+print(random)
+
+
