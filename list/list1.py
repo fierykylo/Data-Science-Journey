9bf8379722bda6799f894075abe8f48cab34a86c
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-24 21:15:37 +0530

    list-1

--- list/list1.py
+++ list/list1.py
@@ -2,8 +2,12 @@ import sys
 
 list = [10, 24, 49, 49, 94, 101, 23, 78, 90, 67]
 max = list[0]
+min = list[0]
 for i in range(len(list)):
     if (list[i] > max):
         max = list[i]
+    if(list[i] < min):
+        min  = list[i]
 
 print(max)
+print(min)
