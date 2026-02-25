82a0f54876961120e1bf0bf2c158573b07d7d531
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-02-25 10:52:25 +0530

    comments

--- collections/set/set2.py
+++ collections/set/set2.py
@@ -4,7 +4,7 @@ b = {3,4,5}
 c = (a | b) # same as a.union(b)
 print(c)
 
-print (a & b)
-print(a - b)
-print(a ^ b)
+print (a & b) #3
+print(a - b) # 1 2
+print(a ^ b) # 1 2 4 5
 #print(a[0]) wont work as set object is not subscriptable 
